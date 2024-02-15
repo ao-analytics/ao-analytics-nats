7931934c4379dbf09d06dd7152e91a30bb4e32b7
veqox <sonnleitner.luki@gmail.com>
2024-02-15 15:04:45 +0100

    refactor: remove market history ingest

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ extern crate dotenv_codegen;
 use tokio::sync::RwLock;
 use std::sync::Arc;
 
-use aodata_models::nats;
+use aodata_models::{db, nats};
 
 use futures_util::StreamExt;
 use sqlx::postgres::PgPoolOptions;
@@ -30,7 +30,6 @@ async fn main() -> Result<(), async_nats::SubscribeError> {
 
     let nats_url = dotenv!("NATS_URL");
     let nats_market_orders_subject = dotenv!("NATS_MARKET_ORDERS_SUBJECT");
-    let nats_market_histories_subject = dotenv!("NATS_MARKET_HISTORIES_SUBJECT");
     let nats_user = dotenv!("NATS_USER").to_string();
     let nats_password = dotenv!("NATS_PASSWORD").to_string();
 
@@ -46,12 +45,7 @@ async fn main() -> Result<(), async_nats::SubscribeError> {
 
     info!("Subscribed to subject {}", nats_market_orders_subject);
 
-    let market_histories_subcriber = client.subscribe(nats_market_histories_subject).await?;
-
-    info!("Subscribed to subject {}", nats_market_histories_subject);
-
-    let market_orders: Arc<RwLock<Vec<nats::MarketOrder>>> = Arc::new(RwLock::new(Vec::new()));
-    let market_histories: Arc<RwLock<Vec<nats::MarketHistories>>> = Arc::new(RwLock::new(Vec::new()));
+    let market_orders: Arc<RwLock<Vec<db::MarketOrder>>> = Arc::new(RwLock::new(Vec::new()));
 
     let market_order_message_handler = tokio::spawn(handle_market_orders_messages(
         market_orders.clone(),
@@ -60,52 +54,39 @@ async fn main() -> Result<(), async_nats::SubscribeError> {
 
     info!("Started market order message handler thread");
 
-    let market_history_message_handler = tokio::spawn(handle_market_histories_messages(
-        market_histories.clone(),
-        pool.clone(),
-    ));
-
-    info!("Started market history message handler thread");
-
-    let mut messages =
-        futures_util::stream::select_all([market_orders_subcriber, market_histories_subcriber]);
+    let mut messages = futures_util::stream::select_all([market_orders_subcriber]);
 
     while let Some(msg) = messages.next().await {
 
-        if msg.subject.as_str() == nats_market_orders_subject {
-            let parse_result = serde_json::from_slice::<nats::MarketOrder>(&msg.payload);
-            
-            let market_order = match parse_result {
-                Ok(market_order) => market_order,
-                Err(err) => {
-                    warn!("Failed to parse market order message: {}", err);
-                    continue;
-                }
-            };
-
-            market_orders.write().await.push(market_order);
+        if msg.subject.as_str() != nats_market_orders_subject {
+            warn!("Unknown subject: {}", msg.subject);
             continue;
         }
+        
+        let parse_result = serde_json::from_slice::<nats::MarketOrder>(&msg.payload);
+        
+        let market_order = match parse_result {
+            Ok(market_order) => market_order,
+            Err(err) => {
+                warn!("Failed to parse market order message: {}", err);
+                continue;
+            }
+        };
 
-        if msg.subject.as_str() == nats_market_histories_subject {
-            let parse_result = serde_json::from_slice::<nats::MarketHistories>(&msg.payload);
+        let market_order = db::MarketOrder::from_nats(&market_order);
 
-            let market_history = match parse_result {
-                Ok(market_history) => market_history,
-                Err(err) => {
-                    warn!("Failed to parse market history message: {}", err);
-                    continue;
-                }
-            };
+        let market_order = match market_order {
+            Some(market_order) => market_order,
+            None => {
+                warn!("Failed to parse market order message");
+                continue;
+            }
+        };
 
-            market_histories.write().await.push(market_history);
-            continue;
-        }
-
-        warn!("Unknown subject: {}", msg.subject);
+        market_orders.write().await.push(market_order);
     }
 
-    _ = tokio::join!(market_order_message_handler, market_history_message_handler);
+    _ = tokio::join!(market_order_message_handler);
 
     pool.close().await;
 
@@ -113,7 +94,7 @@ async fn main() -> Result<(), async_nats::SubscribeError> {
 }
 
 async fn handle_market_orders_messages(
-    market_orders: Arc<RwLock<Vec<nats::MarketOrder>>>,
+    market_orders: Arc<RwLock<Vec<db::MarketOrder>>>,
     pool: Pool<Postgres>,
 ) -> Result<(), async_nats::Error> {
     loop {
@@ -127,7 +108,7 @@ async fn handle_market_orders_messages(
         let start = chrono::Utc::now();
 
         let mut queue_lock = market_orders.write().await;
-        let messages: Vec<nats::MarketOrder> = queue_lock.drain(0..queue_size).collect();
+        let messages: Vec<db::MarketOrder> = queue_lock.drain(0..queue_size).collect();
         drop(queue_lock);
 
         let result = utils::db::insert_market_orders(&pool, messages).await;
@@ -144,44 +125,6 @@ async fn handle_market_orders_messages(
 
         info!(
             "Inserted {} market orders in {} ms",
-            rows_affected,
-            end.signed_duration_since(start).num_milliseconds()
-        );
-    }
-}
-
-async fn handle_market_histories_messages(
-    market_histories: Arc<RwLock<Vec<nats::MarketHistories>>>,
-    pool: Pool<Postgres>,
-) -> Result<(), async_nats::Error> {
-    loop {
-        let queue_size = market_histories.read().await.len();
-
-        if queue_size < 1000 {
-            tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
-            continue;
-        }
-
-        let start = chrono::Utc::now();
-
-        let mut queue_lock = market_histories.write().await;
-        let messages: Vec<nats::MarketHistories>= queue_lock.drain(0..queue_size).collect();
-        drop(queue_lock);
-
-        let result = utils::db::insert_market_histories(&pool, messages).await;
-
-        let rows_affected = match result {
-            Ok(rows_affected) => rows_affected.rows_affected(),
-            Err(err) => {
-                warn!("Failed to insert market histories: {}", err);
-                continue;
-            }
-        };
-
-        let end = chrono::Utc::now();
-
-        info!(
-            "Inserted {} market histories in {} ms",
             rows_affected,
             end.signed_duration_since(start).num_milliseconds()
         );
--- src/utils/db.rs
+++ src/utils/db.rs
@@ -1,9 +1,9 @@
-use aodata_models::{db, nats};
+use aodata_models::db;
 use sqlx::{postgres::PgQueryResult, types::chrono, Pool, Postgres};
 
 pub async fn insert_market_orders(
     pool: &Pool<Postgres>,
-    market_orders: Vec<nats::MarketOrder>,
+    market_orders: Vec<db::MarketOrder>,
 ) -> Result<PgQueryResult, sqlx::Error> {
     let mut ids: Vec<i64> = Vec::new();
     let mut item_unique_names: Vec<String> = Vec::new();
@@ -18,16 +18,9 @@ pub async fn insert_market_orders(
     let mut updated_ats: Vec<chrono::NaiveDateTime> = Vec::new();
 
     for market_order in market_orders.iter().rev() {
-        let market_order = db::MarketOrder::from_nats(&market_order);
-
-        let market_order = match market_order {
-            Some(market_order) => market_order,
-            None => continue,
-        };
-
         ids.push(market_order.id);
-        item_unique_names.push(market_order.item_unique_name);
-        location_ids.push(market_order.location_id);
+        item_unique_names.push(market_order.item_unique_name.clone());
+        location_ids.push(market_order.location_id.clone());
         quality_levels.push(market_order.quality_level);
         enchantment_levels.push(market_order.enchantment_level);
         unit_prices_silver.push(market_order.unit_price_silver);
@@ -110,102 +103,6 @@ WHERE EXISTS (
         &expires_ats,
         &created_ats,
         &updated_ats
-    )
-    .execute(pool)
-    .await;
-
-    match result {
-        Ok(result) => {
-            transaction.commit().await.unwrap();
-            return Ok(result);
-        }
-        Err(e) => {
-            transaction.rollback().await.unwrap();
-            return Err(e);
-        }
-    }
-}
-
-pub async fn insert_market_histories(
-    pool: &Pool<Postgres>,
-    market_histories: Vec<nats::MarketHistories>,
-) -> Result<PgQueryResult, sqlx::Error> {
-    let mut item_unique_names: Vec<String> = Vec::new();
-    let mut location_ids: Vec<String> = Vec::new();
-    let mut quality_levels: Vec<i32> = Vec::new();
-    let mut timescales: Vec<i32> = Vec::new();
-    let mut timestamps: Vec<chrono::NaiveDateTime> = Vec::new();
-    let mut item_amounts: Vec<i32> = Vec::new();
-    let mut silver_amounts: Vec<i32> = Vec::new();
-    let mut created_ats: Vec<chrono::NaiveDateTime> = Vec::new();
-    let mut updated_ats: Vec<chrono::NaiveDateTime> = Vec::new();
-
-    for market_history in market_histories.into_iter().rev() {
-        db::MarketHistory::from_nats(market_history)
-            .unwrap()
-            .iter()
-            .rev()
-            .for_each(|market_history| {
-                if item_unique_names.contains(&market_history.item_id)
-                    && location_ids.contains(&market_history.location_id)
-                    && quality_levels.contains(&market_history.quality_level)
-                    && timescales.contains(&market_history.timescale)
-                    && timestamps.contains(&market_history.timestamp)
-                {
-                    return;
-                }
-
-                item_unique_names.push(market_history.item_id.clone());
-                location_ids.push(market_history.location_id.clone());
-                quality_levels.push(market_history.quality_level);
-                timescales.push(market_history.timescale);
-                timestamps.push(market_history.timestamp);
-                item_amounts.push(market_history.item_amount);
-                silver_amounts.push(market_history.silver_amount);
-                created_ats.push(market_history.created_at);
-                updated_ats.push(market_history.updated_at);
-            });
-    }
-
-    let transaction = pool.begin().await.unwrap();
-
-    let result = sqlx::query!(
-        "
-INSERT INTO market_history (
-    item_unique_name,
-    location_id,
-    quality_level,
-    timescale,
-    timestamp,
-    item_amount,
-    silver_amount,
-    created_at,
-    updated_at)
-SELECT * FROM UNNEST(
-    $1::VARCHAR[],
-    $2::VARCHAR[],
-    $3::INT[],
-    $4::INT[],
-    $5::TIMESTAMP[],
-    $6::INT[],
-    $7::INT[],
-    $8::TIMESTAMP[],
-    $9::TIMESTAMP[])
-ON CONFLICT (item_unique_name, location_id, quality_level, timescale, timestamp, updated_at) DO
-    UPDATE SET
-        updated_at = EXCLUDED.updated_at,
-        silver_amount = EXCLUDED.silver_amount,
-        item_amount = EXCLUDED.item_amount
-        ",
-        &item_unique_names,
-        &location_ids,
-        &quality_levels,
-        &timescales,
-        &timestamps,
-        &item_amounts,
-        &silver_amounts,
-        &created_ats,
-        &updated_ats,
     )
     .execute(pool)
     .await;
